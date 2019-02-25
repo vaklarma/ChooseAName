2d84399fad439a7c9a182990929ecccc43794620
biro.andrasgabor <biro.andrasgabor.regist@gmail.com>
2019-02-25 22:45:05 +0100

    (add) Add About module and routerlink to navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,21 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FooterComponent} from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule
   ],
   declarations: [
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   exports: [
     FooterComponent,
+    NavbarComponent
   ]
 })
 export class CoreModule {
